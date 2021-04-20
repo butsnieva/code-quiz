81aa2585191793184df8f94e8b70f30e94a1b257
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-20 00:58:43 -0400

    add a logic of how the elements shows-up on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 var headerEl = document.querySelector('#header')
 var timeRemainingEl = document.querySelector('#timer')
 var mainEl = document.querySelector('#main')
+var startingEl = document.querySelector('#start-quiz')
 var startBtn = document.querySelector('#start-btn')
 var quizEl = document.querySelector('#quiz-questions')
 var questionEl = document.querySelector('#question')
 var choicesEl = document.querySelector('#choices')
+
 
 var questionId = 0
 var countTime = 60
@@ -54,12 +56,11 @@ var questionArr = [
 ]
     //console.log(questionArr)
 
-
 function countdown() {
     var timerEl = document.createElement('p')
     timeRemainingEl.appendChild(timerEl)
 
-    var interval = setInterval(function() {
+    interval = setInterval(function() {
         if (countTime >= 1) {
             timerEl.textContent = 'Time: ' + countTime + 's'
             countTime--
@@ -74,23 +75,43 @@ startBtn.addEventListener('click', countdown)
 startBtn.addEventListener('click', quiz)
 
 function quiz() {
+    startingEl.innerHTML = ""
     questionEl.textContent = questionArr[questionId].question
         for (var i = 0; i < 4; i++) {
             var choiceBtn = document.createElement('button')
                 choiceBtn.className = 'choice'
                 choiceBtn.textContent = questionArr[questionId].choices[i]
-            choicesEl.appendChild(choiceBtn)
-            choicesEl.setAttribute('id', i)
+                choicesEl.appendChild(choiceBtn)
         }
     choicesEl.addEventListener('click', checkAnswer)
+    questionId++
 }
 
 function checkAnswer(event) {
-    if (event.target.textContent === questionArr[questionId].correct) {
+    choicesEl.innerHTML=''
+    // if (questionId === questionArr.length) {
+    //     score()
+    if (event.target.textContent === questionArr[questionId-1].correct) {
         alert('correct!')
+            if (questionId === questionArr.length) {
+                score()
+            }else{
+                quiz()}
     } else {
         alert('wrong')
         countTime = countTime - 10
-    }
-}
+            if (questionId === questionArr.length) {
+                score()
+            }else{
+                quiz()}
+    //debugger
+}}
 
+
+function score() {
+    quizEl.innerHTML = ''
+    clearInterval(interval)
+    var finalScoreEl = document.createElement('div')
+    finalScoreEl.innerHTML = ('<h2>All Done!</h2><p>Your final score is ' + parseInt(countTime+1) + '!</p><div><p>Enter initials:</p><input type="text"></input><button>Submit</button></div>')
+    mainEl.appendChild(finalScoreEl)
+}
